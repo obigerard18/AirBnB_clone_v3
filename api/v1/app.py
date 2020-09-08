3a7846cfc49b8e832b4a208deeec93274b2355a7
daniela <1469@holbertonschool.com>
2020-09-08 16:12:48 +0000

    Adding tasks from 8 to 10

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,11 +2,12 @@
 """Module for Flask REST_API"""
 from flask import Flask
 from models import storage
+from os import getenv
 from api.v1.views import app_views
-
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+
 
 @app.teardown_appcontext
 def closedb(db_close):
