1ffbf0bc37200c53998949a21de9f94a1a04db7f
monicajoa <mnortiz.ortiz@gmail.com>
2020-09-08 00:38:49 -0500

    AirBnB clone - RESTful API project, mandatory task 5. Some stats, add from os import getenv

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,15 +3,18 @@
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
+from os import getenv
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def closedb(db_close):
     """Closes db session"""
     storage.close()
+
 
 if __name__ == "__main__":
     host = getenv("HBNB_API_HOST", "0.0.0.0")
